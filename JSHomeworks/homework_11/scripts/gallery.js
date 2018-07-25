2867f6a72d4f45043d8b8bc8ece7a57aa9886fc8
h010d <lessonsep@gmail.com>
2018-07-25 17:17:45 +0300

    rwt6r5d867t9

--- JSHomeworks/homework_11/scripts/gallery.js
+++ JSHomeworks/homework_11/scripts/gallery.js
@@ -2,104 +2,56 @@ function Gallery(sSelector) {
     // ***************** свойства ******************* //
     var g = this;
     g.init(sSelector);
-    g.pictures = g.findObj('.b-picture');
-    g.arrowPrev = g.findObj('.b-preview__arrow_prev');
-    g.arrowNext = g.findObj('.b-preview__arrow_next');
-    g.preview = g.findObj('.b-preview');
-    g.previewImage = g.findObj('.b-preview__image');
-    g.previewText = g.findObj('.b-preview__text');
-    g.currentPlace = g.findObj('.b-preview__currentPlace');
-    g.maxPlace = g.findObj('.b-preview__maxPlace');
-    g.slideshowBtn = g.findObj('.b-preview__slideshowBtn');
-    g.slideshowPeriod = 1;
-    //Признак того что работает setInterval()
-    g.slideshowIntervalStart = null;
-    // признак запуска слайдшоу
-    g.slideshowStart = false;
-    g.current = 0;
-    g.max = g.pictures.length;
+    g.pictures = g.findObj('.picture');
+    g.displayCurrent = g.findObj('.display');
+    g.displayNext = g.findObj('.display2');
+    g.button = g.findObj('.button');
+    g.startPosition = 0;
+    g.imageMaxPosition = 5;
+    g.currentImage = '';
+    g.nextImage = '';
+    g.prevImage = '';
+    g.maxCount = 5;
+    g.backgroundSize = "100% 100%";
+
+    var pathOne = 'https://orig00.deviantart.net/b5c3/f/2011/283/4/6/46935a1388497ef3fd180d004b75f8a8-d4cf4cy.png',
+        pathSecond = 'https://orig00.deviantart.net/5a7d/f/2013/311/4/f/miley_cyrus_png_by_eda_asdfg-d6tcthx.png',
+        current = '../images/gallery/0.JPG',
+        next = '../images/gallery/1.JPG',
+        count = 0,
+        timeAnim = 4000;
+
     // ******************* методы ******************* //
-
-    g.showPreview = function() {
-        var currentPicture = $(this);
-        g.current = g.pictures.index(currentPicture); // TODO: посмотреть разницу разобраться
-        g.display(currentPicture);
+    g.startInit = function() {
+        g.currentImage = "url('../images/gallery/" + count++ + ".JPG')";
+        g.nextImage = "url('../images/gallery/" + count++ + ".JPG')";
+        g.displayCurrent.css({ "background-image": g.currentImage, "background-size": g.backgroundSize });
     };
-    g.closePreview = function() {
-        //|| $(event.target)
-        if (!event || $(event.target).hasClass('b-preview')) {
-            g.preview.removeClass('b-preview_shown');
-            g.stopSlideshow();
+    rotation = function() {
+        if (count >= g.maxCount) {
+            count = 0;
         }
-    };
-
-    g.showImage = function(iShift) {
-        g.current += iShift;
-        if (g.current >= g.max) {
-            g.current = 0;
-        } else if (g.current < 0) {
-            g.current = g.max - 1;
-        }
-        g.display(g.pictures.eq(g.current));
-    };
-    g.display = function(oPicture) {
-        var smallImage = oPicture.find('.b-picture__image'),
-            bigImageSrc = smallImage.attr('src').replace('small_', '');
-        g.previewImage.attr('src', bigImageSrc);
-        g.previewText.html(smallImage.attr('alt'));
-        g.preview.addClass('b-preview_shown');
-        g.currentPlace.html(g.current + 1);
-        g.maxPlace.html(g.max);
-    };
-    g.showPrevious = function() {
-        g.showImage(-1);
-
-    };
-    g.showNext = function() {
-        g.showImage(1);
+        g.changeImageCurrent(g.displayCurrent).animate({ opacity: 0.0 }, timeAnim);
+        g.changeImageNext(g.displayNext).animate({ opacity: 1.0 }, timeAnim);
+        g.currentImage = g.nextImage;
+        g.nextImage = "url('../images/gallery/" + count++ + ".JPG')";
 
     };
 
-    g.galleryEvents = function() {
-        console.info(event.which, event.altKey, event.ctrlKey, event.shiftKey);
-        if (event.which == 27) {
-            g.closePreview();
-        }
-        if (event.which == 37) {
-            g.showPrevious();
-        }
-        if (event.which == 39) {
-            g.showNext();
-        }
+    g.changeImageCurrent = function(oObject) {
+        oObject.css({ "background-image": g.currentImage, "background-size": g.backgroundSize, opacity: 1.0 });
+        return oObject;
     };
-    g.slideshow = function() {
-        // g.slideshowStart ? g.slideshowBtn.text('stop') : g.slideshowBtn.text('start');
-        if (g.slideshowStart) {
+    g.changeImageNext = function(oObject) {
+        oObject.css({ "background-image": g.nextImage, opacity: 0.0, "background-size": g.backgroundSize });
+        return oObject;
+    };
 
-            // g.slideshowBtn.text('start');
-            // g.slideshowStart = false;
-            g.stopSlideshow();
-
-        } else {
-            g.setSlideshowData('stop', true);
-            g.slideshowIntervalStart = setInterval(g.showNext, g.slideshowPeriod * 1000);
-        }
-
+    g.show = function() {
+        window.setInterval(rotation, timeAnim * 2);
     };
-    g.stopSlideshow = function() {
-        g.setSlideshowData('start', false);
-        clearInterval(g.slideshowIntervalStart);
-    };
-    g.setSlideshowData = function(btnText, slideshowStart) {
-        g.slideshowBtn.text(btnText);
-        g.slideshowStart = slideshowStart;
-    };
+    g.startInit();
     // ******************* события ******************* //
-    g.slideshowBtn.click(g.slideshow);
-    g.pictures.click(g.showPreview);
-    g.preview.click(g.closePreview);
-    g.arrowPrev.click(g.showPrevious);
-    g.arrowNext.click(g.showNext);
-    $('body').keyup(g.galleryEvents);
+    g.button.click(g.show);
 }
 Gallery.prototype = new Component();
