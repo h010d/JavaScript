efb7e23876d1b311a7e2c41bb0f3957b62fc8458
h010d <lessonsep@gmail.com>
2018-07-26 22:52:51 +0300

    solution true

--- JSHomeworks/homework_11/scripts/testSlider.js
+++ JSHomeworks/homework_11/scripts/testSlider.js
@@ -6,28 +6,34 @@ function Gallery(sSelector) {
     g.display = g.findObj('.display');
     g.buttonStart = g.findObj('.button_start');
     g.backgroundSize = '100% 100%';
+    g.newdiv2 = $("<div class='object2'></div>");
+    g.newdiv1 = $("<div class='object2'></div>");
     g.currentImage = '';
     g.nextImage = '';
     g.prevImage = '';
+    g.time = 1000 * 3;
     var pathSecond = "url(\'https://orig00.deviantart.net/5a7d/f/2013/311/4/f/miley_cyrus_png_by_eda_asdfg-d6tcthx.png\')";
     var pathOne = "url(\'https://orig00.deviantart.net/b5c3/f/2011/283/4/6/46935a1388497ef3fd180d004b75f8a8-d4cf4cy.png\')";
     // ******************* методы  ******************* //
     g.startInit = function() {
-        g.currentImage = 'url(\'../images/gallery/0.JPG\')';
+        // g.currentImage = 'url(\'../images/gallery/0.JPG\')';
         // g.setNumberQueue();
-        g.display.css({
-            'background-image': pathSecond,
-            'background-size': g.backgroundSize
-        });
+        // g.display.css({
+        //     'background-image': pathOne,
+        //     'background-size': g.backgroundSize
+        // });
         // g.setDisplayInfoText();
     };
     g.addBlock = function() {
 
-        var newdiv1 = $("<div class='object1'></div>");
-        var newdiv2 = $("<div class='object2'></div>");
-        // g.display.append(newdiv1, newdiv2);
-        g.display.css({ "background-image": pathSecond });
-        // newdiv2.css({ "background": "url('" + pathOne + "')", opacity: 1.0 });
+        g.display.append(g.newdiv2, g.newdiv1);
+        g.newdiv2.css({ "background": pathOne, 'background-size': g.backgroundSize, 'opacity': 1.0, "height": "200px", "width": "150px", 'position': 'relative' })
+            .animate({ 'opacity': 0.0 }, g.time)
+            .animate({ 'opacity': 1.0 }, g.time);
+        g.newdiv1.css({ "background": pathSecond, 'background-size': g.backgroundSize, 'opacity': 0.0, "height": "200px", "width": "150px", 'position': 'absolute', 'top': '0' })
+            .animate({ 'opacity': 1.0 }, g.time)
+            .animate({ 'opacity': 0.0 }, g.time);
+        // g.display.css({ "background": transparent, "background-image": pathSecond, 'background-size': g.backgroundSize });
 
 
         console.log('g.buttonStart :', g.buttonStart);
