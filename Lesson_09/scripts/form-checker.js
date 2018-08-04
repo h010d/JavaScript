2ffe747eb3bb00fdf1f8d2a678d63ded8f6ba65c
h010d <lessonsep@gmail.com>
2018-08-04 18:30:40 +0300

    end 13 lesson

--- Lesson_09/scripts/form-checker.js
+++ Lesson_09/scripts/form-checker.js
@@ -1,38 +1,38 @@
 function FormChecker(sSelector) {
-	var f = this;
-	f.init(sSelector);
-	f.textfields      = f.findObj('.b-textfield');
-	f.errorMessage    = f.findObj('.b-form__message_error');
-	f.checkTextfield  = function (textfield) {
-		var currentTextfield    = textfield.length ? textfield : $(this),
-		    currentTextfieldVal = currentTextfield.val(),
-		    regexps             = Settings.get('regexps'),
-		    currentRegExp       = regexps[currentTextfield.attr('name')],
-		    oCurrentRegExp      = new RegExp(currentRegExp),
-		    textfieldError      = !currentTextfieldVal.match(oCurrentRegExp);
-		
-		currentTextfield.toggleClass('b-textfield_error', textfieldError);
-		console.log('textfieldError', textfieldError);
-		// console.log('currentRegExp', currentRegExp);
-		return textfieldError;
-		
-	};
-	f.checkTextfields = function (event) {
-		event.preventDefault();
-		var formError = false;
-		f.textfields.each(function () {
-			var currentTextfield = $(this);
-			textfieldError       = f.checkTextfield(currentTextfield);
-			// console.log(textfieldError);
-			if (textfieldError) {
-				formError = true;
-			}
-		});
-		var methodType = formError ? 'slideDown' : 'slideUp';
-		f.errorMessage[methodType]();
-	};
-	f.textfields.blur(f.checkTextfield);
-	f.elem.submit(f.checkTextfields);
+    var f = this;
+    f.init(sSelector);
+    f.textfields = f.findObj('.b-textfield');
+    f.errorMessage = f.findObj('.b-form__message_error');
+    f.checkTextfield = function(textfield) {
+        var currentTextfield = textfield.length ? textfield : $(this),
+            currentTextfieldVal = currentTextfield.val(),
+            regexps = Settings.get('regexps'),
+            currentRegExp = regexps[currentTextfield.attr('name')],
+            oCurrentRegExp = new RegExp(currentRegExp),
+            textfieldError = !currentTextfieldVal.match(oCurrentRegExp);
+
+        currentTextfield.toggleClass('b-textfield_error', textfieldError);
+        console.log('textfieldError', textfieldError);
+        // console.log('currentRegExp', currentRegExp);
+        return textfieldError;
+
+    };
+    f.checkTextfields = function(event) {
+        event.preventDefault();
+        var formError = false;
+        f.textfields.each(function() {
+            var currentTextfield = $(this);
+            textfieldError = f.checkTextfield(currentTextfield);
+            // console.log(textfieldError);
+            if (textfieldError) {
+                formError = true;
+            }
+        });
+        var methodType = formError ? 'slideDown' : 'slideUp';
+        f.errorMessage[methodType]();
+    };
+    f.textfields.blur(f.checkTextfield);
+    f.elem.submit(f.checkTextfields);
 }
 
 FormChecker.prototype = new Component();
---
+++ Lessons_13/scripts/cart.js
@@ -0,0 +1,100 @@
+function Cart(sSelector, sCartSelector) {
+    /* *
+     *findObj() для каталога
+     *find() для корзины
+     */
+    var c = this;
+    c.init(sSelector);
+    c.cart = $(sCartSelector);
+    c.list = c.cart.find('.b-minicart__list'); // список корзины
+    c.button = c.cart.find('.b-minicart__button'); // кнопка корзины
+    c.total = c.cart.find('.b-minicart__total'); //общая
+    c.quantity = c.cart.find('.b-minicart__quantity'); //количество
+    c.goods = {}; /* id : qty */
+    $.cookie.json = true;
+
+    c.add = function(event) {
+        event.preventDefault();
+        var orderForm = $(this);
+        var currentGood = orderForm.closest('.b-good');
+        tagName = currentGood.prop('tagName');
+        console.log('currentGood :', currentGood);
+        // console.log('oSourceElement :', oSourceElement);
+        // console.log('oDestinationElement :', oDestinationElement);
+        console.log('tagName.IMG 0 :', tagName.IMG);
+        console.info('currentGood.attr(\'class\')', currentGood.attr('class'));
+        var addedGood = c.put(currentGood); // * добавляем товар в корзину
+        // console.log('addedGood :', addedGood);
+        // c.goods[addedGood.getId()] = orderForm.find('.b-order-form__quantity').val(); //?
+        // console.log('c.goods :', c.goods);
+        //**установка куки
+        $.cookie('cartGoods', c.goods, { 'expires': 7, 'path': '/' });
+        // разворачивает корзину
+        c.list.stop().slideDown();
+
+    };
+    c.put = function(oCurrentGood) {
+        //?
+        tagName = oCurrentGood.prop('tagName');
+        // console.log('oSourceElement :', oSourceElement);
+        // console.log('oDestinationElement :', oDestinationElement);
+        console.log('tagName.IMG 1 :', tagName.IMG);
+        //?
+        var addedGood = new Good(oCurrentGood);
+        // console.log('addedGood :', addedGood);
+        oCurrentGood.addClass('b-good_in-cart');
+        var goodIdClass = 'b-good_id_' + addedGood.getId();
+        // console.log('goodIdClass :', goodIdClass);
+        var existingGood = c.list.find('.' + goodIdClass);
+        // console.log('existingGood :', existingGood);
+        if (existingGood.length) {
+
+        } else {
+            var newGood = c.list.find('.b-good:first-child').clone();
+            newGood.addClass(goodIdClass);
+            // console.log('newGood :', newGood);
+            c.copyData(oCurrentGood, newGood, [
+                '.b-good__image',
+                '.b-good__name',
+                '.b-order-form__quantity'
+            ]);
+            console.log('newGood :', newGood);
+            c.list.find('.b-goods').append(newGood);
+        }
+        return addedGood;
+    };
+    c.load = function() {
+        //считываем товары из куки
+        c.goods = $.cookie('cartGoods');
+        console.log('load.c.goods  :', c.goods);
+        if (c.goods) {
+            $.each(c.goods, function(goodId, quantity) {
+                var currentGood = c.findObj('.b-good_id_' + goodId);
+                currentGood.find('.b-b-order-form__quantity').val(quantity);
+                c.put(currentGood);
+            });
+        }
+    };
+    c.showHideCart = function() {
+        c.list.stop().slideToggle();
+    };
+    c.changeQuantity = function(oBtn, iShift) {
+        console.log('oBtn :', oBtn);
+        console.log('iShift :', iShift);
+    };
+    c.del = function(param) {};
+    c.showCartInfo = function() {};
+    c.increaseQuantity = function() {
+        c.changeQuantity(this, 1);
+    };
+    c.decreaseQuantity = function() {
+        c.changeQuantity(this, -1);
+    };
+    /* ***************************** */
+    c.load();
+    c.findObj('.b-order-form').submit(c.add);
+    c.button.click(c.showHideCart);
+    c.findObj('.b-order-form__plus').click(c.increaseQuantity);
+    c.findObj('.b-order-form__minus').click(c.decreaseQuantity);
+}
+Cart.prototype = new Component();
