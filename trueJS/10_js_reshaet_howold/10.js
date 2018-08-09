3fa1320ce0c7418970de14d0c5ba1a392968f964
h010d <lessonsep@gmail.com>
2018-08-10 00:05:52 +0300

    xsdfgh

--- trueJS/10_js_reshaet_howold/10.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('calc').onclick = function(){
-	let y = document.getElementById('year').value;
-	let old = 2018 - y;
-	if (old >= 16) alert ('Добро пожаловать');
-	else alert('Ошибка входа');
-}
--- trueJS/13_js_reshaet_cycles/13.js
+++
@@ -1,8 +0,0 @@
-let h = document.getElementsByTagName('a'); // h - коллекция
-
-for (let i = 0; i<h.length; i++){
-	//console.log( h[i].href );
-	if (h[i].getAttribute('href') == '#') {
-		h[i].style.color = 'red';
-	}
-}
